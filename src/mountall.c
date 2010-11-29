bb05d38964086f96d21c27cd9f5d3bcd03a44c70
Colin Watson <cjwatson@canonical.com>
2010-11-29 16:12:20 +0000

    Consider gfs2 filesystems to be remote (thanks, Steve Wilson; LP: #582278).

--- src/mountall.c
+++ src/mountall.c
@@ -979,7 +979,8 @@ is_remote (Mount *mnt)
 	    || (! strcmp (mnt->type, "ncp"))
 	    || (! strcmp (mnt->type, "ncpfs"))
 	    || (! strcmp (mnt->type, "ocfs2"))
-	    || (! strcmp (mnt->type, "gfs"))) {
+	    || (! strcmp (mnt->type, "gfs"))
+	    || (! strcmp (mnt->type, "gfs2"))) {
 		return TRUE;
 	} else {
 		return FALSE;
